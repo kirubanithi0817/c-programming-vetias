18894b81332ce1434cb9b147ccdfc847bcf84ee0
kirubanithi0817 <kirubanithir23sit@vetias.ac.in>
2025-05-12 15:55:46 +0530

    Add files via upload

---
+++ CEO/day 1 maths.c
@@ -0,0 +1,22 @@
+
+#include <stdio.h>
+
+int main()
+{
+ int a,b,sum,sub,mul,divi;
+ printf("enter value a:");
+ scanf("%d",&a);
+ printf("enter value b:");
+ scanf("%d",&b);
+ sum=a+b;
+ printf("sum of %d and %d is:%d",a,b,sum);
+ sub=a-b;
+ printf("\nsub of %d and %d is:%d",a,b,sub);
+ mul=a*b;
+ printf("\nmul of %d and %d is:%d",a,b,mul);
+ divi=a/b;
+ printf("\ndivi of %d and %d is:%d",a,b,divi);
+ 
+
+    return 0;
+}
